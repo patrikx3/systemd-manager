228ff87e1920cb96feeb418b9fe0b35ab2b8b683
patrikx3 <alabard@gmail.com>
2021-06-13 10:13:54 +0200

    r0b08x [chore]   6/13/2021, 10:13:54 AM

--- src/dbus/systemd.js
+++ src/dbus/systemd.js
@@ -4,7 +4,6 @@ const DBus = require('dbus');
 //gdbus introspect --system --dest org.freedesktop.systemd1 --object-path /org/freedesktop/systemd1
 //https://www.freedesktop.org/wiki/Software/systemd/dbus/
 //https://github.com/Shouqun/node-dbus
-let bus
 const defaults = {
     _interface: {
         system: 'org.freedesktop.systemd1',
@@ -28,16 +27,16 @@ const getInterface = async (
     system = defaults._interface.system) => {
 
     process.env.DISPLAY = settings.dbus.display;
-    if (process.env.DBUS_SESSION_BUS_ADDRESS === undefined) {
+//    if (process.env.DBUS_SESSION_BUS_ADDRESS === undefined) {
         process.env.DBUS_SESSION_BUS_ADDRESS = settings.dbus.address;
-    }
+//    }
 
-    if (bus === undefined) {
-        bus = DBus.getBus('system')
+    if (global.p3xBus === undefined) {
+        global.p3xBus = DBus.getBus('system')
     }
 
     return new Promise((resolve, reject) => {
-        bus.getInterface(system, node, _interface, (error, manager) => {
+        global.p3xBus.getInterface(system, node, _interface, (error, manager) => {
             if (error) {
                 return reject(error);
             }
